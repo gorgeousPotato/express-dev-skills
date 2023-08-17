d912248080bd36b50df40b48b90ef35d96427c2b
gorgeousPotato <lanauzbekov@gmail.com>
2023-08-16 23:20:22 -0700

    implements delete functionality

--- models/skill.js
+++ models/skill.js
@@ -40,6 +40,7 @@ module.exports = {
   getAll,
   getOne,
   create,
+  deleteOne,
 };
 
 function getAll() {
@@ -55,4 +56,10 @@ function getOne(id) {
 function create(skill) {
   skill.id = Date.now() % 1000000;
   skills.push(skill);
+}
+
+function deleteOne(id) {
+  id = parseInt(id);
+  const idx = skills.findIndex(skill => skill.id === id);
+  skills.splice(idx, 1);
 }
