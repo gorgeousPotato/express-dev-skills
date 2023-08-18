c376728a4fe6428090e29cb4e167408e2c14d4a0
gorgeousPotato <lanauzbekov@gmail.com>
2023-08-17 22:12:11 -0700

    implements edit and update functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -6,6 +6,8 @@ module.exports = {
   new: newSkill,
   create,
   delete: deleteSkill,
+  edit,
+  update,
 };
 
 function index(req, res) {
@@ -36,4 +38,17 @@ function create(req, res) {
 function deleteSkill(req, res) {
   Skill.deleteOne(req.params.id);
   res.redirect("/skills");
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render("skills/edit", {
+    title: "Edit Skill",
+    skill,
+  });
+}
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
 }
--- models/skill.js
+++ models/skill.js
@@ -4,35 +4,36 @@ const skills = [
     name: "JavaScript",
     description:
       "JavaScript is a high-level, often just-in-time compiled language that conforms to the ECMAScript standard. It has dynamic typing, prototype-based object-orientation, and first-class functions. It is multi-paradigm, supporting event-driven, functional, and imperative programming styles. It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM).",
-    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     id: 232301,
     name: "HTML",
     description:
       "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the meaning and structure of web content. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.",
-    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
   },
   {
     id: 232302,
     name: "CSS",
     description:
       "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML ",
-    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    id: 232306,
+    name: "Figma",
+    description:
+      "Figma is a collaborative web application for interface design",
   },
   {
     id: 232303,
     name: "Debugging",
     description:
       "Debugging is the process of finding and fixing errors or bugs in the source code of any software. ",
-    link: "https://en.wikipedia.org/wiki/Debugging",
   },
   {
     id: 232304,
     name: "Problem-solving",
     description:
       "Problem solving is the act of defining a problem; determining the cause of the problem; identifying, prioritizing, and selecting alternatives for a solution; and implementing a solution.",
-    link: "https://en.wikipedia.org/wiki/Problem_solving",
   },
 ];
 
@@ -41,6 +42,7 @@ module.exports = {
   getOne,
   create,
   deleteOne,
+  update,
 };
 
 function getAll() {
@@ -62,4 +64,10 @@ function deleteOne(id) {
   id = parseInt(id);
   const idx = skills.findIndex(skill => skill.id === id);
   skills.splice(idx, 1);
+}
+
+function update(id, newSkill) {
+  id = parseInt(id);
+  const skill = skills.find(skill => skill.id === id);
+  Object.assign(skill, newSkill);
 }
